612cbd59a0fc1bdae2435dae7f6cdf2cfa0312eb
Luiz Mantuano <luizmantuano.dev@gmail.com>
2025-01-27 21:05:15 -0300

    refactor: standardize string quotes and improve code readability in Home.tsx

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -6,33 +6,33 @@ import {
   Vibration,
   Keyboard,
   Pressable,
-} from 'react-native';
-import React, { useState } from 'react';
-import * as Animatable from 'react-native-animatable';
-import styles from './styles';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import ResultImc from '../../components/ResultImc';
-import { useNavigation } from '@react-navigation/native';
-import { DrawerNavigationProp } from '@react-navigation/drawer';
-import { DrawerParamsType, Screens } from '../../routes/types';
-import { TextInputMask } from 'react-native-masked-text';
+} from "react-native";
+import React, { useState } from "react";
+import * as Animatable from "react-native-animatable";
+import styles from "./styles";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import ResultImc from "../../components/ResultImc";
+import { useNavigation } from "@react-navigation/native";
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import { DrawerParamsType, Screens } from "../../routes/types";
+import { TextInputMask } from "react-native-masked-text";
 
 type NavigationType = DrawerNavigationProp<DrawerParamsType, Screens.HOME>;
 
 const Home = (props) => {
   const [weight, setWeight] = useState(null);
   const [height, setHeight] = useState(null);
-  const [messageImc, setMessageImc] = useState('preencha o peso e altura');
+  const [messageImc, setMessageImc] = useState("preencha o peso e altura");
   const [imc, setImc] = useState(null);
-  const [textButton, setTextButton] = useState('calcular');
+  const [textButton, setTextButton] = useState("calcular");
   const [errorMessage, setErrorMessage] = useState(null);
-  const [infoVerification, setInfoVerification] = useState('');
+  const [infoVerification, setInfoVerification] = useState("");
   const { navigate } = useNavigation<NavigationType>();
 
   //--> CÁLCULO IMC <--//
 
   function imcCalculator() {
-    let heightFormat = height.replace(',', '.');
+    let heightFormat = height.replace(",", ".");
     return setImc((weight / (heightFormat * heightFormat)).toFixed(2));
   }
   //------------------------------------------------------------//
@@ -42,10 +42,9 @@ const Home = (props) => {
   function verificationImc() {
     if (imc === null) {
       Vibration.vibrate();
-      setErrorMessage('campo obrigatório *');
+      setErrorMessage("campo obrigatório *");
     }
   }
-  //------------------------------------------------------------//
 
   function goInformativeScreen() {
     navigate(Screens.INFORMATION, { imc });
@@ -58,50 +57,54 @@ const Home = (props) => {
       imcCalculator();
       setHeight(null);
       setWeight(null);
-      setMessageImc('Seu imc é igual: ');
-      setTextButton('Calcular Novamente');
+      setMessageImc("Seu imc é igual: ");
+      setTextButton("Calcular Novamente");
       setErrorMessage(null);
-      setInfoVerification('O que Significa?');
+      setInfoVerification("O que Significa?");
     } else {
-      setInfoVerification('');
+      setInfoVerification("");
       verificationImc();
       setImc(null);
-      setTextButton('Calcular');
-      setMessageImc('preencha o peso e altura');
+      setTextButton("Calcular");
+      setMessageImc("preencha o peso e altura");
     }
   }
   //------------------------------------------------------------//
 
   return (
     <Pressable onPress={Keyboard.dismiss} style={styles.container}>
-      <Animatable.View animation='bounceInDown' style={styles.AreaTitle}>
+      <Animatable.View animation="bounceInDown" style={styles.AreaTitle}>
         <TouchableOpacity
           style={styles.Btn}
           onPress={() => props.navigation.openDrawer()}
         >
-          <Ionicons name='menu' size={25} color='#F1F1F1' />
+          <Ionicons name="menu" size={25} color="#F1F1F1" />
         </TouchableOpacity>
         <Text style={styles.Title}>Calcule seu IMC</Text>
       </Animatable.View>
       <View style={styles.AreaTxt}>
         <Text style={styles.txt}>
-          O IMC é um cálculo simples que permite {'\n'} avaliar se a pessoa está dentro do
-          peso que {'\n'}é considerado ideal para a sua altura.
+          O IMC é um cálculo simples que permite {"\n"} avaliar se a pessoa está
+          dentro do peso que {"\n"}é considerado ideal para a sua altura.
         </Text>
       </View>
-      <Animatable.View animation='fadeInUpBig' delay={600} style={styles.AreaInput}>
+      <Animatable.View
+        animation="fadeInUpBig"
+        delay={600}
+        style={styles.AreaInput}
+      >
         <Text style={styles.TxtInput}>Altura</Text>
         <Text style={styles.errorMessage}>{errorMessage}</Text>
         <TextInputMask
-          type='custom'
+          type="custom"
           options={{
-            mask: '9.99',
+            mask: "9.99",
           }}
           style={styles.input}
           onChangeText={setHeight}
           value={height}
-          placeholder='Ex 1.65'
-          keyboardType='numeric'
+          placeholder="Ex 1.65"
+          keyboardType="numeric"
         />
         <Text style={styles.TxtInput}>Peso</Text>
         <Text style={styles.errorMessage}>{errorMessage}</Text>
@@ -109,17 +112,23 @@ const Home = (props) => {
           style={styles.input}
           onChangeText={setWeight}
           value={weight}
-          placeholder='Ex 70.65'
-          keyboardType='numeric'
+          placeholder="Ex 70.65"
+          keyboardType="numeric"
         />
 
-        <TouchableOpacity style={styles.AreaBtn} onPress={() => validationImc()}>
+        <TouchableOpacity
+          style={styles.AreaBtn}
+          onPress={() => validationImc()}
+        >
           <Text style={styles.BtnTxt}>{textButton}</Text>
         </TouchableOpacity>
         <ResultImc messageResultImc={messageImc} resultImc={imc} />
         <View style={styles.AreaLine}>
           <View style={styles.LineViewLeft} />
-          <TouchableOpacity style={styles.AreaInfo} onPress={goInformativeScreen}>
+          <TouchableOpacity
+            style={styles.AreaInfo}
+            onPress={goInformativeScreen}
+          >
             <Text style={styles.InfoTxt}>{infoVerification}</Text>
           </TouchableOpacity>
           <View style={styles.LineViewRight} />
